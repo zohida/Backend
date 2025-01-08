7b11df8a9d41a5b55450896674f7fe7d17a207a5
Zohida <zohishka04@gmail.com>
2025-01-08 05:22:18 -0800

    new Users entering

--- routes/auth.js
+++ routes/auth.js
@@ -11,11 +11,18 @@ const SECRET_KEY = process.env.SECRET_KEY;
 
 router.post("/register", async (req,res) => {
     try {
+      
+        const {username, email, password} = req.body;
+        
         if (!username || !email || !password) {
           return res.status(400).json({ message: "All fields are required" });
         }
-        const {username, email, password} = req.body;
-
+    
+        // Email takrorlanish xatosini tekshirish
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return res.status(400).json({ message: "Email already exists" });
+        }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
