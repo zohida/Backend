24d166f1b4fb632ea4bb2aaaa7b947a10f76a675
Zohida <zohishka04@gmail.com>
2025-01-08 10:20:15 -0800

    Fixed url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("uploads", express.static(path.join(__dirname, "/uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 let url = process.env.MONGO_URI;
 
